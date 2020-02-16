37c73dc72c8004769c893b2e3eb6537dc702eae0
ralphi2811 <ralphi2811@gmail.com>
2020-02-16 20:16:58 +0400

    Add status function

--- index.php
+++ index.php
@@ -1,24 +1,37 @@
 <?php
 
 require_once 'NissanConnect.class.php';
+require_once 'config/nissanlogin.php';
+
+$UserLogin = new NissanLogin();
+$nissanConnect = new NissanConnect($UserLogin->Login(), $UserLogin->Password(), 'America/New_York', NissanConnect::COUNTRY_EU, NissanConnect::ENCRYPTION_OPTION_OPENSSL);
 
 $action = $_GET['action'];
 $param = $_GET['param'];
+$waitForResult = FALSE; 
 
-if ($param = 'clim') {
+if ($param === 'clim') {
  
     switch ($action) {
         case 'on':
+            $nissanConnect->startClimateControl($waitForResult);
             echo 'Allumage de la clim';
             break;
             
         case 'off':
+            $nissanConnect->stopClimateControl($waitForResult);
             echo 'Extinction de la clim';
             break;
             
         default :
             echo 'Parametre incorrect';
+            break;
         }
     
+} elseif ($param === 'status') {
+    
+    $result = $nissanConnect->getStatus();
+    echo var_dump($result);
+
 }
 
